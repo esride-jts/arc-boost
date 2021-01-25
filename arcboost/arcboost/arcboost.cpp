770e86059e9bfc287d07c9739b018137a507b665
Jan Tschada <jts@esri-de.com>
2021-01-25 11:54:21 +0100

    Added a simple startup cmake project with license

---
+++ arcboost/arcboost/arcboost.cpp
@@ -0,0 +1,12 @@
+﻿// arcboost.cpp : Defines the entry point for the application.
+//
+
+#include "arcboost.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello arcboost." << endl;
+	return 0;
+}
